674220c5bd1fdacf6d34a34c3eb6ca2db393d967
arcticsnow <sfihol@alaska.edu>
2018-01-24 17:54:57 +0100

    first data

---
+++ analysis/exploring_20180123.py
@@ -0,0 +1,13 @@
+from __future__ import division
+import os, sys, sqlite3
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Include class and function here
+
+
+
+# Include script in this if statement
+if __name__ == '__main__':
